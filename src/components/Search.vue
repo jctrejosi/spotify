494a0f5d550d15871ec771945005d47040e0e719
Juan Trejos <jctrejosi@unal.edu.co>
2022-02-07 10:15:41 -0500

    search component

---
+++ src/components/Search.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="search">
+    <label for="user" class="search__label">
+      <input
+        class="search__input"
+        placeholder="Artistas, canciones o podcasts"
+        v-model="search"
+        type="text"
+      />
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Search",
+
+  data() {
+    return {
+      search: "",
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.search {
+  display: flex;
+  align-items: center;
+  width: 100%;
+  padding: 0 2rem;
+  height: 4rem;
+
+  &__label {
+    height: 60%;
+    width: 30%;
+  }
+
+  &__input {
+    border-radius: 2rem;
+    background-image: url("search.svg");
+    background-repeat: no-repeat;
+    background-position: 4px center;
+    background-size: 20px;
+    padding: 0 2rem;
+    border: 0.1rem solid $dark-color;
+    height: 100%;
+    width: 100%;
+  }
+}
+</style>
--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -1,23 +1,27 @@
 <template>
   <div class="main-container">
     <LeftMenu />
+    <Search />
   </div>
 </template>
 
 <script>
 import LeftMenu from "../components/LeftMenu/LeftMenu.vue";
+import Search from "../components/Search.vue";
 
 export default {
   name: "MainPage",
 
   components: {
     LeftMenu,
+    Search,
   },
 };
 </script>
 
 <style lang="scss" scoped>
 .main-container {
+  display: flex;
   width: 100%;
   height: 100%;
   background-color: $bg-dark-nearly-color;
